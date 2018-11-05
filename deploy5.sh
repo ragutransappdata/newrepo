9e7873ea37f5c538db8cbc5bb5233d7536146ce1
ragutransappdata <44695632+ragutransappdata@users.noreply.github.com>
2018-11-05 10:33:19 +0530

    fifth commit

--- deploy5.sh
+++ deploy5.sh
@@ -1,8 +1,10 @@
 . /u03/env/ORCL.env
 which sqlplus
 sqlplus system/passwd  <<EOF
+WHENEVER SQLERROR EXIT SQL.SQLCODE;
 select sysdate+1 from dual;
 SELECT username FROM dba_users WHERE username='RAGHU';
 DROP table RAGHU.name;
 EOF
-exit
+export VAR1=`echo $?`
+exit $VAR1
